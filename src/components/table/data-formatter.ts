c1d84892528ae3223e17bf2a2e6b5128ab8eca10
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-24 21:48:29 +0300

    separate data formatter

---
+++ src/components/table/data-formatter.ts
@@ -0,0 +1,18 @@
+import { OrganizedMBData } from '@/types/organized-mb-data';
+import { MBData } from '@/types/mb-data';
+
+export const dataFormatter: (data: MBData[]) => OrganizedMBData[] = (data: MBData[]) => {
+  return data.map((item: MBData) => {
+    return {
+      uploaded_variation: item['main.uploaded_variation'],
+      existing_variation: item['main.existing_variation'],
+      symbol: item['main.symbol'],
+      af_vcf: item['main.af_vcf'],
+      dp: item['main.dp'],
+      dann_score: item['details2.dann_score'],
+      pheno_pubmed: item['links.pheno pubmed'],
+      mondo: item['links.mondo'],
+      provean: item['details2.provean'],
+    };
+  });
+};
--- src/components/table/mb-table.tsx
+++ src/components/table/mb-table.tsx
@@ -4,24 +4,16 @@ import { useReactTable, getCoreRowModel } from '@tanstack/react-table';
 import { columns } from './columns';
 import MBTableHeader from './mb-table-header';
 import MBTableRow from './mb-table-row';
-import { OrganizedMBData } from '@/types/organized-mb-data';
 import { MBData } from '@/types/mb-data';
 
+import { dataFormatter } from './data-formatter';
+
 export default function MBTable({ data }: { data: MBData[] }): JSX.Element {
-  const organizedData: OrganizedMBData[] = data.map((item: MBData) => {
-    return {
-      uploaded_variation: item['main.uploaded_variation'],
-      existing_variation: item['main.existing_variation'],
-      symbol: item['main.symbol'],
-      af_vcf: item['main.af_vcf'],
-      dp: item['main.dp'],
-      dann_score: item['details2.dann_score'],
-      pheno_pubmed: item['links.pheno pubmed'],
-      mondo: item['links.mondo'],
-      provean: item['details2.provean'],
-    };
+  const table = useReactTable({
+    data: dataFormatter(data),
+    columns,
+    getCoreRowModel: getCoreRowModel(),
   });
-  const table = useReactTable({ data: organizedData, columns, getCoreRowModel: getCoreRowModel() });
   return (
     <Table>
       <TableHeader>
