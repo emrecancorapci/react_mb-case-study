f0290c5816a8d95c185989c9f5cd80c07c8ad7d3
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-24 19:03:55 +0300

    change layout

--- src/app/main-layout.tsx
+++ src/app/main-layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="flex w-full flex-col items-center justify-between">
       <Header />
-      <main className="my-10 grid w-full grid-cols-6 flex-row justify-between gap-2">
+      <main className="my-10 flex w-full justify-center">
         <Outlet />
       </main>
     </div>
