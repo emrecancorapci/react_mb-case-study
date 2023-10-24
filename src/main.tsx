c281000080fce4f3b4b4d9c21b945699722b4f3f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-24 17:48:16 +0300

    add tanstack query provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import { RouterProvider } from 'react-router-dom';
 import ThemeProvider from './providers/theme-provider.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './router.tsx';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
