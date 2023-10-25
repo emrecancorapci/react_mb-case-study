2708f2aed49b852d20fe4c941561abd65bf2ed15
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-10-25 12:45:26 +0300

    separate data formatter and buttons from table

--- src/app/mb-table.tsx
+++ src/app/mb-table.tsx
@@ -1,18 +1,16 @@
 // eslint-disable-next-line import/named
 import { getCoreRowModel, useReactTable } from '@tanstack/react-table';
 
-import { MBData } from '@/types/mb-data';
+import { OrganizedMBData } from '@/types/organized-mb-data';
 
 import { columns } from '../components/table/columns';
-import { dataFormatter } from '../components/table/data-formatter';
 import MBTableHeader from '../components/table/mb-table-header';
 import MBTableRow from '../components/table/mb-table-row';
-import { Button } from '../components/ui/button';
 import { Table } from '../components/ui/table';
 
-export default function MBTable({ data }: { data: MBData[] }): JSX.Element {
+export default function MBTable({ data }: { data: OrganizedMBData[] }): JSX.Element {
   const table = useReactTable({
-    data: dataFormatter(data),
+    data: data,
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
@@ -23,14 +21,6 @@ export default function MBTable({ data }: { data: MBData[] }): JSX.Element {
           <MBTableHeader table={table} />
           <MBTableRow table={table} columns={columns} />
         </Table>
-      </div>
-      <div className="flex flex-row justify-end gap-4 p-4">
-        <Button onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-          Önceki
-        </Button>
-        <Button onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-          Sonraki
-        </Button>
       </div>
     </>
   );
