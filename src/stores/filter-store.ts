74596aa9ad3711d9e3af18578de0f64b5333b8aa
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-11-01 03:30:38 +0300

    fix sorting

--- src/stores/filter-store.ts
+++ src/stores/filter-store.ts
@@ -132,7 +132,6 @@ export const useFilterStore = create<FilterStore>((set) => ({
 
       return {
         sorting: {
-          ...state.sorting,
           [newSorting as FormattedDataType]: newSortingValue,
         } as Sorting | undefined,
       };
